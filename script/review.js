7965dbe81c0611a4355da22cdeb6c841b9488be8
Kyara <kyara0324@naver.com>
2024-05-07 17:44:53 +0900

    86%

--- script/review.js
+++ script/review.js
@@ -14,10 +14,17 @@ function toggleLike(index) {
   loadComments(); // 댓글 다시 불러와서 UI 업데이트
 }
 
+const URLSearch = new URLSearchParams(location.search);
+
+function searchParam(key) {
+  return URLSearch.get(key); //query string으로 담아놓고 영화id값을 get해서 가져오기
+}
+
 // 댓글 저장 함수
 function saveComments() {
   const comments = JSON.parse(localStorage.getItem("comments") || "[]"); // 저장된 댓글을 배열로 파싱하거나, 없으면 새 배열 할당
   const newComment = {
+    movieId: searchParam("id"),
     id: Id.value,
     password: password.value,
     comment: comment.value,
@@ -31,15 +38,16 @@ function saveComments() {
 function loadComments() {
   reviewDiv.innerHTML = ""; // 기존 댓글창 초기화
   const comments = JSON.parse(localStorage.getItem("comments") || "[]");
-  comments.forEach((comment, index) => {
+  const commentFilter = comments.filter((comment) => {
+    return comment.movieId === searchParam("id");
+  });
+  commentFilter.forEach((comment, index) => {
     let liked = comment.liked || false;
     const reviewHtml = `
         <div class="commentBox">
           <div class="profileCircle"></div>
-          
           <div class="profileBox">
           <div class="nickname">${comment.id}</div>
-            
             <img class="heart_image" src=${
               liked ? "../img/heart_full.png" : "../img/heart.png"
             } onclick= "toggleLike(${index})" alt="#">
@@ -48,7 +56,6 @@ function loadComments() {
                comment.time
              ).toLocaleString()}</div> <!--댓글 시간 출력-->
             <div class="profileEdit">
-             
               <div class="textBox">
               <button class="btn_design" onclick="editComment(${index})">수정</button> <!-- 수정 이벤트 -->
               <button class="btn_design" onclick="deleteComment(${index})">삭제</button> <!-- 삭제 이벤트 -->
@@ -81,7 +88,12 @@ window.onload = function () {
 //  댓글 삭제 구현
 function deleteComment(index) {
   const comments = JSON.parse(localStorage.getItem("comments") || "[]");
-  const commentToDelete = comments[index];
+  console.log(index);
+  const commentDlt = comments.filter((comment, idx) => {
+    return comment.movieId === searchParam("id");
+  });
+
+  const commentToDelete = commentDlt[index]; //index에 뭐가 오는지 주석으로 달기
 
   const inputPassword = prompt("비밀번호를 입력하세요.");
   if (inputPassword === null) {
@@ -90,8 +102,12 @@ function deleteComment(index) {
   }
 
   if (inputPassword === commentToDelete.password) {
-    comments.splice(index, 1); // 해당 인덱스의 댓글 삭제
-    localStorage.setItem("comments", JSON.stringify(comments)); // 로컬 스토리지에 저장
+    // commentDlt.splice(index, 1); // 해당 인덱스의 댓글 삭제   <----★★★ 얘 대신 filter
+    const f = comments.filter((comment) => {
+      return comment.comment !== commentToDelete.comment;
+    });
+
+    localStorage.setItem("comments", JSON.stringify(f)); // 로컬 스토리지에 저장
     loadComments(); // 업데이트된 댓글을 다시 로드
     alert("댓글이 삭제되었습니다.");
   } else {
